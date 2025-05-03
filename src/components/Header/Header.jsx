ed2fecd781e0661d8a8cfe925c02766fbcb0f41e
Anna Kršková <krskova.aaa@gmail.com>
2025-05-03 15:06:09 +0200

    Komponenty, zprovozneni navigace a napoj jako komponenta

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,30 @@
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <header id="home">
+      <div className="header__content container">
+        <div className="site-logo"></div>
+
+        <div className="navigation">
+          <button className="nav-btn"></button>
+          <nav className="rollout-nav nav-closed">
+            <a href="#home">domů</a>
+            <a href="#menu">menu</a>
+            <a href="#gallery">galerie</a>
+            <a href="#contact">kontakt</a>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+// Funkcionalita menu
+document.addEventListener('DOMContentLoaded', () => {
+  const navBtn = document.querySelector('.nav-btn');
+  const nav = document.querySelector('nav');
+  navBtn?.addEventListener('click', () => {
+    nav?.classList.toggle('nav-closed');
+  });
+});
